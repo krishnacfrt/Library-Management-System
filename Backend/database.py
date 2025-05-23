0c03851edccaf088dd91c43deb143259312971cb
Krishna <krishna.cfrt@gmail.com>
2025-05-23 21:06:35 +0530

    added database

---
+++ Backend/database.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+db= mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='Krishna4321!',
+    database='library_management'
+      )
+
+cur= db.cursor()
+
+#print(db.connection_id)
+#database and table > users, books, transactions is already created in sql
