f236bd8040d40baf2c376811dc5f52479bd664cc
Krishna <krishna.cfrt@gmail.com>
2025-05-24 13:04:31 +0530

    Added Books api for adding book and getting book list

--- Backend/Utils/Book.py
+++ Backend/Utils/Book.py
@@ -1,2 +1,25 @@
-class Book:
-    pass
+from pydantic import BaseModel
+from datetime import datetime
+from database import cur, db
+
+
+class Book(BaseModel):
+    title: str
+    bookId: int
+    published_date: str
+    author: str
+
+    def save_to_db(self):
+        query = "insert into books (bookId, title, author, published_date) values (%s, %s, %s, %s)"
+        date_object = datetime.strptime(self.published_date, "%Y-%m-%d").date()
+        val = (self.bookId, self.title, self.author, date_object)
+        cur.execute(query, val)
+        db.commit()
+        return "added book successfully"
+
+
+def getBooks():
+    query = "select * from books"
+    cur.execute(query)
+    books = cur.fetchall()
+    return books
--- Backend/main.py
+++ Backend/main.py
@@ -1,17 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from Utils.User import User
 from Utils.util import get_users
+from Utils.Book import Book, getBooks
 
-app= FastAPI()
+app = FastAPI()
 
-@app.post("/users/")
-async def create_user(user: User, satus_code: 200):
+
+@app.post("/users", status_code=status.HTTP_201_CREATED)
+async def create_user(user: User):
     user.save_to_db()
     return {"message": "User added to the database", "user": user}
 
 
-@app.get("/users/")
+@app.get("/users", status_code=status.HTTP_200_OK)
 async def getUsers():
-    users= get_users()
+    users = get_users()
     return {"message": "Users fetched successfully", "users": users}
 
+
+@app.post("/book", status_code=status.HTTP_201_CREATED)
+async def add_book(book: Book):
+    msg = book.save_to_db()
+    return {"message": msg}
+
+
+@app.get("/books", status_code=status.HTTP_200_OK)
+async def get_books():
+    res = getBooks()
+    return {"message": "Books fetched successfully", "books": res}
