e78ab0a427852b358cab75d3d13812f8955cb44b
Krishna <krishna.cfrt@gmail.com>
2025-05-30 08:40:10 +0530

    Cleaned App.jsx

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -11,9 +11,7 @@ function App() {
     <>
       <BrowserRouter>
         <ToastContainer />
-        <div>
-          <Navbar />
-        </div>
+        <Navbar />
         <AppRoutes />
       </BrowserRouter>
     </>
