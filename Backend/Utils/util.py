029f55906c4e77b33340f643c01bd7e13aa5378c
Krishna <krishna.cfrt@gmail.com>
2025-06-06 15:35:27 +0530

    Added config, and made user insertion and deletion working

--- Backend/Utils/util.py
+++ Backend/Utils/util.py
@@ -1,7 +1,22 @@
-from database import cur
+from database import cur, db
 
 
 def get_users():
     cur.execute("SELECT * FROM users")
     users = cur.fetchall()
     return users
+
+
+def validate_user(userId):
+    query = f"select * from users where userId = {userId}"
+    cur.execute(query)
+    res = cur.fetchall()
+    if res:
+        return True
+    return False
+
+
+def remove_user(userId):
+    query = f"DELETE FROM users WHERE userId= {userId}"
+    cur.execute(query)
+    db.commit()
--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, HTTPException
 from Utils.User import User
-from Utils.util import get_users
+from Utils.util import get_users, validate_user, remove_user
 from Utils.Book import Book, getBooks
 
 app = FastAPI()
@@ -13,6 +13,16 @@ async def create_user(user: User):
     details = {"userDetail": user}
     details["userId"] = userId
     return {"message": "User added to the database", "user": details}
+
+
+@app.delete("/user/{user_id}")
+async def delete_user(user_id: int):
+    isValidUser = validate_user(user_id)
+    if isValidUser:
+        remove_user(user_id)
+        return {"status": "OK", "message": "User deleted successfully"}
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
 
 
 @app.get("/users", status_code=status.HTTP_200_OK)
