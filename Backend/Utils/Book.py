515394cd5c06d12d8966e81a58e5695ca58f0557
Krishna <krishna.cfrt@gmail.com>
2025-06-06 23:15:11 +0530

    refactored books table and /books api

--- Backend/Utils/Book.py
+++ Backend/Utils/Book.py
@@ -5,21 +5,52 @@ from database import cur, db
 
 class Book(BaseModel):
     title: str
-    bookId: int
     published_date: str
     author: str
+    standard: int
+    category: str
 
     def save_to_db(self):
-        query = "insert into books (bookId, title, author, published_date) values (%s, %s, %s, %s)"
-        date_object = datetime.strptime(self.published_date, "%Y-%m-%d").date()
-        val = (self.bookId, self.title, self.author, date_object)
+        query = "insert into newBooks ( title, author, published_date, standard, category) values ( %s, %s, %s,%s, %s)"
+        # date_object = datetime.strptime(self.published_date, "%Y-%m-%d").date()
+        val = (
+            self.title,
+            self.author,
+            self.published_date,
+            self.standard,
+            self.category,
+        )
         cur.execute(query, val)
         db.commit()
         return "added book successfully"
 
 
 def getBooks():
-    query = "select * from books"
+    query = "select * from newBooks"
     cur.execute(query)
     books = cur.fetchall()
     return books
+
+
+def drptable():
+    cur.execute("DROP TABLE books;")
+
+
+def createTableBook():
+    query = """CREATE TABLE IF NOT EXISTS newBooks (
+            bookid INT PRIMARY KEY AUTO_INCREMENT,
+            title VARCHAR(255), 
+            author VARCHAR(255),
+            standard VARCHAR(50),
+            category VARCHAR(50)
+        )"""
+
+    cur.execute(query)
+    db.commit()
+
+
+def addBook():
+    query = "insert into newBooks (title, author, standard, category) values (%s, %s, %s, %s)"
+    temp = ("new book", "krishna", 3, "phys")
+    cur.execute(query, temp)
+    db.commit()
