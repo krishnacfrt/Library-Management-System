fd5bbb757564503a8854273b69dcf4f19ddc48c0
Abhishek Kushwaha <abhikush012@gmail.com>
2025-06-08 13:31:47 +0530

    studentTablePreview

--- UI/src/Components/StudentDashboard.jsx
+++ UI/src/Components/StudentDashboard.jsx
@@ -8,17 +8,12 @@ function StudentDashbaord() {
     fontSize: "2.5rem",
     fontWeight: "bold",
     color: "#2c3e50",
-    fontFamily: "Segoe UI, Tahoma, Geneva, Verdana, sans-serif",
-    animation: "fadeIn 1s ease-in-out",
-  };
-
-  const containerStyle = {
-    animation: "fadeIn 1s ease-in-out",
+    fontFamily: "Roboto",
   };
   
   return (
     <>
-      <div style={containerStyle}>
+      <div>
         <h1 style={headingStyle}>Welcome to Student Page</h1>
       </div>
       <div>
