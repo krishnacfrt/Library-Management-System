eb88e3f23bf92f77085f24ec0e70fbb32d4d6837
Krishna <krishna.cfrt@gmail.com>
2025-06-21 17:02:13 +0530

    added docker and requirement.txt

--- Backend/Utils/database.py
+++ Backend/Utils/database.py
@@ -1,11 +1,22 @@
+# import mysql.connector
+
+
+# db = mysql.connector.connect(
+#     host="localhost",
+#     user="root",
+#     password="Krishna4321!",
+#     database="library_management",
+# )
+
+import os
 import mysql.connector
 
-
 db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Krishna4321!",
-    database="library_management",
+    host=os.environ.get("DB_HOST"),
+    user=os.environ.get("DB_USER"),
+    password=os.environ.get("DB_PASSWORD"),
+    database=os.environ.get("DB_NAME"),
+    port=int(os.environ.get("DB_PORT", 3306)),
 )
 
 cur = db.cursor(dictionary=True)
