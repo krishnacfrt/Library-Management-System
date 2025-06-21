a6f47b817bfd69dbccca35b8d962cdf36ee7894c
Krishna <krishna.cfrt@gmail.com>
2025-06-22 01:52:28 +0530

    ref db

--- Backend/Utils/database.py
+++ Backend/Utils/database.py
@@ -1,25 +1,16 @@
-# import mysql.connector
-
-
-# db = mysql.connector.connect(
-#     host="localhost",
-#     user="root",
-#     password="Krishna4321!",
-#     database="library_management",
-# )
-
 import os
+from dotenv import load_dotenv
 import mysql.connector
+from mysql.connector import Error
+
+load_dotenv()
+
 
 db = mysql.connector.connect(
-    host=os.environ.get("DB_HOST"),
-    user=os.environ.get("DB_USER"),
-    password=os.environ.get("DB_PASSWORD"),
-    database=os.environ.get("DB_NAME"),
-    port=int(os.environ.get("DB_PORT", 3306)),
+    host=os.getenv("MYSQL_HOST"),
+    port=int(os.getenv("MYSQL_PORT", 3306)),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    database=os.getenv("MYSQL_DATABASE"),
 )
-
 cur = db.cursor(dictionary=True)
-
-# print(db.connection_id)
-# database and table > users, books, transactions is already created in sql
